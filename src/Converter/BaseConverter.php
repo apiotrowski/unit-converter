7bdbaa1ef78216d6620d48efa7fc66ecf670f0c2
apiotrowski <kontakt@stronawww.net>
2016-10-17 08:34:38 +0200

    Move to BaseConverted methods what are in the interface, in each converters.

--- src/Converter/BaseConverter.php
+++ src/Converter/BaseConverter.php
@@ -2,15 +2,15 @@
 
 namespace UnitConverter\Converter;
 
+use UnitConverter\Exception\NotSupportedConversionException;
+use UnitConverter\Resolver\Query;
 use UnitConverter\Unit\Unit;
+use UnitConverter\Value\Value;
 
 abstract class BaseConverter implements Converter
 {
     /**
-     * @param Unit $sourceUnit
-     * @param Unit $targetUnit
-     * 
-     * @return bool
+     * @inheritdoc
      */
     public function isSupported(Unit $sourceUnit, Unit $targetUnit) : bool
     {
@@ -18,5 +18,41 @@ abstract class BaseConverter implements Converter
             return false;
         }
         return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function convertTo(Value $value, Unit $targetUnit) : Value
+    {
+        if (false === $this->isSupported($value->getUnit(), $targetUnit)) {
+            throw new NotSupportedConversionException($value->getUnit(), $targetUnit);
+        }
+
+        return $this->calculateValue($value, $targetUnit);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function convertFromQuery(Query $query) : Value
+    {
+        return $this->convertTo($query->getValue(), $query->getTargetUnit());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function calculateValue(Value $value, Unit $targetUnit) : Value
+    {
+        $conversionMap = $targetUnit->getConvertUnitMap();
+
+        $valueToConvert = $value->getValue();
+        $valueUnitValue = $conversionMap[(string)$value->getUnit()];
+        $targetUnitValue = $conversionMap[(string)$targetUnit];
+
+        $convertedValue = bcdiv(bcmul($valueToConvert, $targetUnitValue, 5), $valueUnitValue, 2);
+
+        return new Value($convertedValue, $targetUnit);
     }
 }
--- src/Converter/WeightConverter.php
+++ src/Converter/WeightConverter.php
@@ -11,58 +11,10 @@ use UnitConverter\Value\Value;
 class WeightConverter extends BaseConverter
 {
     /**
-     * @return array
+     * @inheritdoc
      */
     public function supportedUnits() : array
     {
         return WeightUnit::$unitList;
-    }
-
-    /**
-     * @param Value $value
-     * @param Unit $targetUnit
-     *
-     * @return Value
-     *
-     * @throws NotSupportedConversionException
-     */
-    public function convertTo(Value $value, Unit $targetUnit) : Value
-    {
-        if (false === $this->isSupported($value->getUnit(), $targetUnit)) {
-            throw new NotSupportedConversionException($value->getUnit(), $targetUnit);
-        }
-
-        return $this->calculateValue($value, $targetUnit);
-    }
-
-    /**
-     * @param Query $query
-     *
-     * @return Value
-     *
-     * @throws NotSupportedConversionException
-     */
-    public function convertFromQuery(Query $query) : Value
-    {
-        return $this->convertTo($query->getValue(), $query->getTargetUnit());
-    }
-
-    /**
-     * @param Value $value
-     * @param Unit $targetUnit
-     *
-     * @return Value
-     */
-    protected function calculateValue(Value $value, Unit $targetUnit) : Value
-    {
-        $conversionMap = $targetUnit->getConvertUnitMap();
-
-        $valueToConvert = $value->getValue();
-        $valueUnitValue = $conversionMap[(string)$value->getUnit()];
-        $targetUnitValue = $conversionMap[(string)$targetUnit];
-
-        $convertedValue = bcdiv(bcmul($valueToConvert, $targetUnitValue, 5), $valueUnitValue, 2);
-
-        return new Value($convertedValue, $targetUnit);
     }
 }
